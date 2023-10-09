8fce00ed0e86c4659af2cc77721dcd12425c0c4a
TakutoJibiki <jibikitakuto@gmail.com>
2023-10-09 12:33:25 +0900

    途中経過をファイルに出力するメソッドを追加
    ◍ 不要な処理を削除

--- RealCodedGeneticAlgorithm.py
+++ RealCodedGeneticAlgorithm.py
@@ -1,142 +1,6 @@
 import numpy as np
-
-class RealCodecGA_JGG_SPX:
-    '''
-    実数値遺伝的アルゴリズム（生存選択モデルJGG、交叉SPX）
-
-    初期化
-    交叉する個体の選択
-    交叉
-    残す個体の選択
-    '''
-    def __init__(self, gene_num, 
-                 initial_min, initial_max, 
-                 population, 
-                 crossover_num, child_num,
-                 evaluation_func, 
-                 seed=None):
-        #乱数シード
-        np.random.seed(seed=seed)
-
-        # 遺伝子情報
-        self.gene_num = gene_num
-
-        #初期化
-        self.initial_min = initial_min
-        self.initial_max = initial_max
-
-        # 交叉する個体の選択
-        self.population = population
-        self.crossed_num = crossover_num
-
-        # 交叉
-        self.child_num = child_num
-
-        # 残す個体の選択
-        # evaluation function defined as following.
-        #  func(genes):
-        #    return evaluations
-        self.evaluation_func = evaluation_func
-
-        #
-        self.genes = None # 候補の遺伝子
-        self.evals = None # 候補の遺伝子の評価値
-        self.best_gene = None # 現時点で評価値が一番高い遺伝子
-        self.best_evaluation = None # 現時点で一番高い評価値
-
-        #
-        self.__initialize_genes()
-
-        return
-
-    def __initialize_genes(self):
-        # 遺伝子の初期値設定
-        # genes = [[gene_0], [gene_1], ... ,[gene_population]]
-        self.genes = self.initial_min + (self.initial_max - self.initial_min) * np.random.rand(self.population, self.gene_num)
-        
-        # 遺伝子の評価値
-        self.evals = self.evaluation_func(self.genes)
-        
-        # min
-        min_idx = np.argmin(self.evals)
-        self.best_evaluation = self.evals[min_idx]
-        self.best_gene = self.genes[min_idx]
-
-        return
-
-    def generation_step(self):
-        # 個体数
-        pop = len(self.genes)
-
-        # 交叉する個体の選択
-        # 交叉する個体のインデックス
-        crossed_idx = self.__random_select(pop, self.crossed_num)
-
-        # 交叉
-        child_genes = self.__simplex_crossover(self.genes[crossed_idx], self.child_num)
-
-        #残す個体の選択
-        survive_genes, survive_evals = self.__ranking_survival(child_genes, self.evaluation_func, survive_num=self.crossed_num)
-        #
-        self.genes[crossed_idx] = survive_genes
-        self.evals[crossed_idx] = survive_evals
-
-        # 最高評価の更新
-        if self.best_evaluation > np.min(survive_evals):
-            min_idx = np.argmin(survive_evals)
-            self.best_evaluation = survive_evals[min_idx]
-            self.best_gene = survive_genes[min_idx]
-
-        return
-
-    def calc_diversity(self):
-        dvs = np.average(np.std(self.genes, axis=0))
-        return dvs
-
-    @staticmethod
-    def __random_select(population, select_num):
-        selected_idx = np.random.choice(np.arange(population), select_num, replace=False)
-        return selected_idx
-
-    @staticmethod
-    def __simplex_crossover(genes, child_num):
-        # 交叉される遺伝子数
-        crss_num = len(genes)
-
-        # expansion_rate
-        expansion_rate = np.sqrt(crss_num + 1.0)
-
-        # Qk計算, Q_(k-1) = e * (P_(k-1) - P_(k)), k=1,2,...,n
-        # n = crss_num-1
-        diff_genes = np.diff(genes, n=1, axis=0) * (-1.0) * expansion_rate
-
-        # R行列計算
-        # [[uniform]*(crsvr_num-1)]*child_num
-        rand_coef = np.random.rand(child_num, crss_num-1)
-        rand_coef = np.power(rand_coef, 1.0/(np.arange(crss_num-1)+1.0))
-        # [u_0*u_1*...*u_n, u_1*...*u_n, ... , u_(n-1)*u_n, u_n
-        rand_coef = np.cumprod(rand_coef[:,::-1], axis=1)[:,::-1]
-
-        # Cn計算
-        Cn = np.dot(rand_coef, diff_genes)
-
-        # xn計算
-        center_gravity = np.average(genes, axis=0)
-        xn = center_gravity + expansion_rate * (genes[-1] - center_gravity)
-
-        # C計算
-        child_genes = xn + Cn
-
-        return child_genes
-
-    @staticmethod
-    def __ranking_survival(genes, evaluation_func, survive_num):
-        evals = evaluation_func(genes)
-        survive_idx = np.argpartition(evals, survive_num)[:survive_num]
-        #
-        survive_genes = genes[survive_idx]
-        survive_eval = evals[survive_idx]
-        return survive_genes, survive_eval
+import os, shutil
+import pandas as pd
 
 class RealCodecGA_JGG_AREX:
     '''
@@ -161,6 +25,8 @@ class RealCodecGA_JGG_AREX:
          initial_expantion_rate = 1.0
          learning_rate = 1/5/gene_num
         '''
+        # 結果削除
+        if os.path.isdir('result'): shutil.rmtree('result')
 
         #乱数シード
         np.random.seed(seed=seed)
@@ -207,6 +73,7 @@ class RealCodecGA_JGG_AREX:
         self.best_gene = None # 現時点で評価値が一番高い遺伝子
         self.best_evaluation = None # 現時点で一番高い評価値
         self.last_gene = None # 最終時点での遺伝子
+        self.generation = 0 # 世代数
         
         #
         self.__initialize_genes()
@@ -217,7 +84,7 @@ class RealCodecGA_JGG_AREX:
         # 遺伝子の初期値設定
         # genes = [[gene_0], [gene_1], ... ,[gene_population]]
         self.genes = self.initial_min + (self.initial_max - self.initial_min) * np.random.rand(self.population, self.gene_num)
-        
+
         # 遺伝子の評価値
         self.evals = self.evaluation_func(self.genes)
         
@@ -229,6 +96,8 @@ class RealCodecGA_JGG_AREX:
         return
 
     def generation_step(self):
+        self.generation += 1
+
         # 個体数
         pop = len(self.genes)
 
@@ -264,6 +133,17 @@ class RealCodecGA_JGG_AREX:
     def calc_diversity(self):
         dvs = np.average(np.std(self.genes, axis=0))
         return dvs
+    
+    def save_result(self):
+        if not os.path.isdir('result'): os.makedirs('result')
+        if not os.path.isdir('result/population'): os.makedirs('result/population')
+        if not os.path.isdir('result/elite_img'): os.makedirs('result/elite_img')
+        pd.DataFrame(self.genes).to_csv(
+            f'result/population/{self.generation}.csv', index=None
+        )
+        with open('result/obj_func.csv', 'a') as f: f.write(f'{self.best_evaluation}\n')
+        with open('result/elite.csv', 'a') as f: f.write(f'{",".join(str(i) for i in self.best_gene)}\n')
+
 
     @staticmethod
     def __random_select(population, select_num):
@@ -316,49 +196,6 @@ class RealCodecGA_JGG_AREX:
         survive_eval = evals[survive_idx]
         return survive_genes, survive_eval, survive_idx
 
-class Sample:
-    @staticmethod
-    def sample_RealCodecGA_JGG_SPX():
-        # define evaluation func
-        def minus_l2(ndar):
-            ev = - np.sum(np.square(ndar), axis=1)
-            return ev
-        # rcga class
-        rcga = RealCodecGA_JGG_SPX(gene_num=2, 
-                                   initial_min=-1, initial_max=1, 
-                                   population=10, 
-                                   crossover_num=3, child_num=10,
-                                   evaluation_func=minus_l2, 
-                                   seed=1000)
-
-        # optimize
-        for i in range(100):
-            rcga.generation_step()
-            print('{0}'.format(i+1))
-            print(' best evals : {0}'.format(rcga.best_evaluation))
-            print(' best genes : {0}'.format(rcga.best_gene))
-        return
-    @staticmethod
-    def sample_RealCodecGA_JGG_AREX():
-        # define evaluation func
-        def minus_l2(ndar):
-            ev = - np.sum(np.square(ndar), axis=1)
-            return ev
-        # rcga class
-        rcga = RealCodecGA_JGG_AREX(gene_num=2, 
-                                    evaluation_func=minus_l2, 
-                                    initial_min=-1, initial_max=1, 
-                                    population=10, 
-                                    crossover_num=3, child_num=6, 
-                                    initial_expantion_rate=1.0, learning_rate=0.05, 
-                                    seed=1000)
-        # optimize
-        for i in range(100):
-            rcga.generation_step()
-            print('{0}'.format(i+1))
-            print(' best evals : {0}'.format(rcga.best_evaluation))
-            print(' best genes : {0}'.format(rcga.best_gene))
-        return
 
 if __name__ == '__main__':
     #test()
