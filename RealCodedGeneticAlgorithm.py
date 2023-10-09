efef003ce065f4fe625ca4925581df8a275f5e5e
TakutoJibiki <jibikitakuto@gmail.com>
2023-10-10 00:42:50 +0900

    集団サイズの指定を必須にして他を推奨値固定にした
    固定にする推奨値は親個体数，子個体数，拡張率，学習率

--- RealCodedGeneticAlgorithm.py
+++ RealCodedGeneticAlgorithm.py
@@ -7,22 +7,24 @@ class RealCodecGA_JGG_AREX:
     
     実数値遺伝的アルゴリズム（生存選択モデルJGG、交叉SPX）
 
+    Note:
+        集団サイズ population は問題に応じて 5n ~ 50n を目安に設定
+        
+        ［参考］
+            https://st1990.hatenablog.com/entry/2019/04/21/212326
+            https://github.com/statsu1990/Real-coded-genetic-algorithm
+
     '''
-    def __init__(self, gene_num, evaluation_func, initial_min, initial_max,
-                 population=None, crossover_num=None,child_num=None,
-                initial_expantion_rate=None, learning_rate=None, seed=None,
-                result_path='result'):
-        #乱数シード
+    def __init__(self, gene_num, evaluation_func, population, seed=None, result_path='result'):
+        # 乱数シード
         np.random.seed(seed=seed)
 
         # 結果削除
         self._initialize_result(result_path)
 
         # メンバの初期化
-        self._initialize_attributes(gene_num, evaluation_func, initial_min,
-                                    initial_max, population, crossover_num,
-                                    child_num, initial_expantion_rate, learning_rate)
-        
+        self._initialize_attributes(gene_num, evaluation_func, population)
+
         # 集団の初期化
         self._initialize_genes()
 
@@ -33,55 +35,42 @@ class RealCodecGA_JGG_AREX:
         self.result_path = result_path
         if os.path.isdir(self.result_path): shutil.rmtree(self.result_path)
         
-    def _initialize_attributes(self, gene_num, 
-                 evaluation_func, 
-                 initial_min, initial_max,
-                 population=None, 
-                 crossover_num=None, child_num=None, 
-                 initial_expantion_rate=None, learning_rate=None, ):
-        # 遺伝子情報
+    def _initialize_attributes(self, gene_num, evaluation_func, population):
+        # 遺伝子サイズ
         self.gene_num = gene_num
 
-        # 個体の評価関数
-        # evaluation function defined as following.
-        #  func(genes):
-        #    return evaluations
+        # 評価関数
         self.evaluation_func = evaluation_func
 
         #遺伝子の初期値範囲
-        self.initial_min = initial_min
-        self.initial_max = initial_max
+        self.initial_min = -1.0
+        self.initial_max = 1.0
 
-        # 全個体数
-        # 交叉する個体数
-        if (population is not None) and (crossover_num is not None):
-            self.population = population
-            self.crossed_num = crossover_num
-        elif (population is None) and (crossover_num is not None):
-            self.crossed_num = crossover_num
-            self.population = self.crossed_num
-        elif (population is not None) and (crossover_num is None):
-            self.population = population
-            self.crossed_num = np.minimum(gene_num+1, population)
-        elif (population is None) and (crossover_num is None):
-            self.crossed_num = gene_num+1
-            self.population = self.crossed_num
+        # 集団サイズ
+        assert population is not None
+        self.population = population
 
-        # 交叉して生成する子個体数
-        self.child_num = child_num if child_num is not None else 4*self.crossed_num
+        # 親個体の数
+        self.parent_num = gene_num+1
+
+        # 子個体の数
+        self.child_num = 4*self.gene_num
 
         # 拡張率
-        self.expantion_rate = initial_expantion_rate if initial_expantion_rate is not None else 1.0
-        # 拡張率の学習率
-        self.learning_rate = learning_rate if learning_rate is not None else 1.0/5.0 / gene_num
+        self.expantion_rate = 1.0
 
-        #
-        self.genes = None # 候補の遺伝子
-        self.evals = None # 候補の遺伝子の評価値
-        self.best_gene = None # 現時点で評価値が一番高い遺伝子
-        self.best_evaluation = None # 現時点で一番高い評価値
-        self.last_gene = None # 最終時点での遺伝子
-        self.generation = 0 # 世代数
+        # 拡張率の学習率
+        self.learning_rate = 1.0/(5.0*gene_num)
+
+        self.genes = None               # 集団
+        self.evals = None               # 集団の評価値
+        self.best_gene = None           # エリート個体
+        self.best_evaluation = None     # エリート個体の評価値
+        self.last_gene = None           # 最終時点での遺伝子
+        self.generation = 0             # 世代数
+
+        print(f'gene_num : {self.gene_num}, pop : {self.population}, parent : {self.parent_num}, child : {self.child_num}')
+        exit()
 
     def _initialize_genes(self):
         # 遺伝子の初期値設定
@@ -105,24 +94,22 @@ class RealCodecGA_JGG_AREX:
 
         # 交叉する個体の選択
         # 交叉する個体のインデックス
-        crossed_idx = self._random_select(pop, self.crossed_num)
+        crossed_idx = self._random_select(pop, self.parent_num)
 
         # 交叉
         child_genes, rand_mtrx = self._arex_crossover(self.genes[crossed_idx], self.evals[crossed_idx], self.child_num, self.expantion_rate)
 
         # 残す個体の選択
-        survive_genes, survive_evals, survive_idx = self._ranking_survival(child_genes, self.evaluation_func, survive_num=self.crossed_num)
+        survive_genes, survive_evals, survive_idx = self._ranking_survival(child_genes, self.evaluation_func, survive_num=self.parent_num)
         # 親個体と変更
         self.genes[crossed_idx] = survive_genes.copy()
         self.evals[crossed_idx] = survive_evals
 
         # 拡張率の更新
-        self.expantion_rate = self._update_arex_expantion_rate(self.expantion_rate, self.learning_rate, rand_mtrx[survive_idx], crss_num=self.crossed_num)
+        self.expantion_rate = self._update_arex_expantion_rate(self.expantion_rate, self.learning_rate, rand_mtrx[survive_idx], crss_num=self.parent_num)
         #print(self.expantion_rate)
 
         min_idx = np.argmin(survive_evals)
-        # 最終遺伝子の更新
-        self.last_gene = survive_genes[min_idx].copy()
         # 生き残ったうちの最高評価
         best_survive_evals = survive_evals[min_idx]
         # 最高評価の更新
@@ -132,10 +119,6 @@ class RealCodecGA_JGG_AREX:
         
         return best_survive_evals
 
-    def calc_diversity(self):
-        dvs = np.average(np.std(self.genes, axis=0))
-        return dvs
-    
     def save_result(self):
         if not os.path.isdir(self.result_path): os.makedirs(self.result_path)
         if not os.path.isdir(f'{self.result_path}/population'): os.makedirs(f'{self.result_path}/population')
@@ -193,16 +176,7 @@ class RealCodecGA_JGG_AREX:
     def _ranking_survival(genes, evaluation_func, survive_num):
         evals = evaluation_func(genes)
         survive_idx = np.argpartition(evals, survive_num)[:survive_num]
-        #
+        
         survive_genes = genes[survive_idx].copy()
         survive_eval = evals[survive_idx]
         return survive_genes, survive_eval, survive_idx
-
-
-if __name__ == '__main__':
-    #test()
-    import verification
-    
-    #verification.test_Minimize_L2_1()
-    verification.test_LinearLeastSquares_withRCGA()
-    
